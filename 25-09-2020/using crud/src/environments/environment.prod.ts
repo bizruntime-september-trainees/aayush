f2eb8f655110d40a6c1e48b202d8d43e19a53bb1
aayush2468 <kumar.ayush2468@gmail.com>
2020-09-25 18:40:25 +0530

    ionic app using crud

---
+++ 25-09-2020/using crud/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyD8JMr_4FYhVrRIXweqHqItVdW610IhSVA",
+    authDomain: "crudapp-2834d.firebaseapp.com",
+    databaseURL: "https://crudapp-2834d.firebaseio.com",
+    projectId: "crudapp-2834d",
+    storageBucket: "crudapp-2834d.appspot.com",
+    messagingSenderId: "967905815012",
+    appId: "1:967905815012:web:87cf84084ecb5fdf52fff1"
+  }
+};
